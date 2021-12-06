c7f8f1dc4c27248405791b92b2c5741fc7f6dba4
Arianna Johnson <ariannajohnson588@gmail.com>
2021-12-06 12:35:02 -0500

    flipcard data

--- src/components/CardContainer/index.js
+++ src/components/CardContainer/index.js
@@ -1,16 +1,22 @@
 import React from "react";
 import Flipcard from "../Flipcard";
 import data from '../../data/airlines.json';
+import './CardContainer.css';
 
 const CardContainer = () => {
-    const airlineCard = data.airlines.map(airline => {
+    const airlineCard = data.airlines.map((airline, index) => {
         return (
-            <Flipcard name={airline.name}/>
+            <Flipcard
+            key={index}
+            name={airline.name}
+            phone={airline.phone}
+            website={airline.site}
+            />
         )
     })
 
     return (
-        <div>
+        <div className='cardContainer'>
             {airlineCard}
         </div>
     )
